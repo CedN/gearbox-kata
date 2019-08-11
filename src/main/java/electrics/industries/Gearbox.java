86f9c1feb1a4158102f3dffc4b1c7d5345677927
Cédric Nagat <cnagat@holo.home>
2019-08-11 20:12:10 +0200

    Removes magic numbers

--- src/main/java/electrics/industries/Gearbox.java
+++ src/main/java/electrics/industries/Gearbox.java
@@ -2,20 +2,26 @@ package electrics.industries;
 
 public class Gearbox {
 
+    private static final int HIGHER_SHIFT_POINT = 2000;
+    private static final int LOWER_SHIFT_POINT = 500;
+    private static final int NEUTRAL_SPEED = 0;
+    private static final int HIGHEST_SPEED = 6;
+    private static final int LOWEST_SPEED = 1;
+
     private int speed = 0;
     private int enginePower = 0;
 
     public void processEnginePower(int enginePower) {
-        if (speed > 0) {
-            if (enginePower > 2000) {
+        if (speed > NEUTRAL_SPEED) {
+            if (enginePower > HIGHER_SHIFT_POINT) {
                 speed++;
-            } else if (enginePower < 500) {
+            } else if (enginePower < LOWER_SHIFT_POINT) {
                 speed--;
             }
         }
-        if (speed > 6) {
+        if (speed > HIGHEST_SPEED) {
             speed--;
-        } else if (speed < 1) {
+        } else if (speed < LOWEST_SPEED) {
             speed++;
         }
         this.enginePower = enginePower;
